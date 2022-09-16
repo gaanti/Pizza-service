c543a9f49c333ccd16364470b0895bcaef67a371
gaanti <gaskevich06@gmail.com>
2022-09-17 02:42:15 +0300

    Frontend is ready

--- src/redux/services/api.ts
+++ src/redux/services/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:8080' });
+const baseQuery = fetchBaseQuery({ baseUrl: 'http://hope-it-will-work.eu-north-1.elasticbeanstalk.com' });
 export const api = createApi({
       reducerPath: 'pizzaApi',
       baseQuery: baseQuery,
