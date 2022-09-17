7fcf168fda07529a1b8318aea7b9dd2fb34d4114
gaanti <gaskevich06@gmail.com>
2022-09-17 03:24:58 +0300

    Fixing severve...

--- src/redux/services/api.ts
+++ src/redux/services/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://hope-it-will-work.eu-north-1.elasticbeanstalk.com' });
+const baseQuery = fetchBaseQuery({ baseUrl: 'http://hope-it-will-work.eu-north-1.elasticbeanstalk.com' });
 export const api = createApi({
       reducerPath: 'pizzaApi',
       baseQuery: baseQuery,
