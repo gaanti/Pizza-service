2f7c5723cbf817fdcce94ac2252d877262f495df
gaanti <gaskevich06@gmail.com>
2022-10-22 21:59:10 +0300

    Add spinner on stripe page loading

--- src/UI/pages/cart/OrderDetails/checkout/stripe-checkout-button.tsx
+++ src/UI/pages/cart/OrderDetails/checkout/stripe-checkout-button.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import { PizzaForCart } from '../../../../../redux/types';
-import { useDoCheckoutMutation } from '../../../../../redux/services/order';
-import './stripe-checkout-button.scss';
+import React from "react";
+import { PizzaForCart } from "../../../../../redux/types";
+import { useDoCheckoutMutation } from "../../../../../redux/services/order";
+import "./stripe-checkout-button.scss";
+import { ImSpinner2 } from "react-icons/im";
 
 function StripeCheckoutButton(props: { pizzas: PizzaForCart[] }) {
-      const [temp, result] = useDoCheckoutMutation();
-      const makeCheckout = async () => {
-            console.log(result);
-            const stripeUrl = await temp(props.pizzas).then((res) => {
-                  console.log(res);
-                  // @ts-ignore
-                  return res.data;
-            });
-            if (stripeUrl) window.location.href = stripeUrl;
-      };
-      return (
-            <button className="stripe-checkout-button" onClick={makeCheckout}>
-                  Checkout with Stripe
-            </button>
-      );
+  const [temp, result] = useDoCheckoutMutation();
+  const makeCheckout = async () => {
+    console.log(result.isLoading);
+    const stripeUrl = await temp(props.pizzas).then((res) => {
+      console.log(res);
+      // @ts-ignore
+      return res.data;
+    });
+    if (stripeUrl) window.location.href = stripeUrl;
+  };
+  return (
+    <div>
+      {(!result.isLoading) ?
+      <button className="stripe-checkout-button" onClick={makeCheckout}>
+        Checkout with Stripe
+      </button> : <ImSpinner2 className="spining-animation" size="25"/>}
+    </div>
+  )
+
 }
 
 export default React.memo(StripeCheckoutButton);
