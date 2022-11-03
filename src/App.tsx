2d3797561ecee0d0e53583e1cf8648229b6ec4bd
gaanti <gaskevich06@gmail.com>
2022-11-03 12:08:15 +0200

    Added AD script everywhere

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,11 @@ function App() {
 function Wrapper() {
       return (
             <ThemeProvider theme={theme}>
+                  <script
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8893380620489182"
+                        crossOrigin="anonymous"
+                  />
                   <div className="app-wrapper" id="main-page">
                         <div className="background-color">
                               <Outlet />
